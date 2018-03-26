21bc747c6696614c4fe02abd5bf356c0404fae23
Gaurav <graheja1997@gmail.com>
2018-03-27 01:47:41 +0530

    Change in one py file

--- src/app.py
+++ src/app.py
@@ -2,8 +2,9 @@ from flask import Flask,render_template
 
 from src.common.database import Database
 from src.models.person.person import Person
+import src.config as config
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(config)
 app.secret_key = "123"
 
 from src.models.person.views import person_blueprint
