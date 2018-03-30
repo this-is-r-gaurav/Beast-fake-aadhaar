1d6ebe9d04b03ff486339a86a5e5d3d202ce1018
Gaurav <graheja1997@gmail.com>
2018-03-31 03:49:54 +0530

    Testing repo set

--- src/common/database.py
+++ src/common/database.py
@@ -6,14 +6,6 @@ class Database:
 
     @staticmethod
     def initialize():
-        client = pymongo.MongoClient(Database._URI)import pymongo
-import os
-class Database:
-    _URI = os.environ.get('DB_URI')
-    _DATABASE = None
-
-    @staticmethod
-    def initialize():
         client = pymongo.MongoClient(Database._URI)
         Database._DATABASE = client.get_default_database()
 
@@ -41,30 +33,4 @@ class Database:
     def update(collection,query, data):
         return True if Database._DATABASE[collection].update(query,data,upsert = True) else False
 
-
-        Database._DATABASE = client.get_default_database()
-
-    @staticmethod
-    def insert(collection, data):
-        Database._DATABASE[collection].insert(data)
-
-    @staticmethod
-    def delete(collection, query):
-        Database._DATABASE[collection].remove(query)
-
-    @staticmethod
-    def find(collection, query):
-        return Database._DATABASE[collection].find(query)
-
-    @staticmethod
-    def find_one(collection, query):
-        return Database._DATABASE[collection].find_one(query)
-
-    @staticmethod
-    def count(collection,query):
-        return Database._DATABASE[collection].count(query)
-
-    @staticmethod
-    def update(collection,query, data):
-        Database._DATABASE[collection].update(query,data,upsert = True)
 
