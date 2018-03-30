c705829ed0b02a946673a8f614576b3aecf573a8
Gaurav <graheja1997@gmail.com>
2018-03-30 08:38:37 +0530

    url alter

--- src/models/api/api.py
+++ src/models/api/api.py
@@ -1,4 +1,4 @@
-from flask import jsonify,request
+from flask import jsonify, request
 from flask.blueprints import Blueprint
 from flask_restplus import Resource, Api
 
@@ -7,34 +7,34 @@ from src.models.api.methods import APIMethods
 from src.models.person.person import Person
 
 api_blueprint = Blueprint('api', __name__)
-api = Api(api_blueprint,'1.0','Testing Beast Aadhaar Api', 'This is a Fake Aadhar Api for SIH Hackathon', default_label='Beast Fake Aadhaar',default='Beast')
+api = Api(api_blueprint, '1.0', 'Testing Beast Aadhaar Api', 'This is a Fake Aadhar Api for SIH Hackathon',
+          default_label='Beast Fake Aadhaar', default='Beast')
 
 
 @api.route('/users')
 class ListAllUser(Resource):
     def get(self):
         cluster_data = APIMethods.get_cluster_user()
-        return jsonify({'data': cluster_data })
+        return jsonify({'data': cluster_data})
+
 
 @api.route('/aadhaar')
 class GetUserInfo(Resource):
     @api.doc(params={
         'aadhaar_no': {'in': 'formData', 'description': 'User Aadhaar Number', 'required': 'True'}})
     def post(self):
-        aadhaar_no=request.form['aadhaar_no']
+        aadhaar_no = request.form['aadhaar_no']
         person = Person.get_by_aadhaar(aadhaar_no)
-	if person:
+        if person:
             return jsonify(
                 {
-                "aadhaar": person.aadhaar_no,
-                "name": person.name,
-                "gender": person.gender,
-                "address": person.address,
-                "dob": person.dob.strftime("%Y-%m-%d"),
-                "phone": person.phone,
-                "image": domain+"static/assets/images/"+person.image
-                 })
-	else:
+                    "aadhaar": person.aadhaar_no,
+                    "name": person.name,
+                    "gender": person.gender,
+                    "address": person.address,
+                    "dob": person.dob.strftime("%Y-%m-%d"),
+                    "phone": person.phone,
+                    "image": domain + "static/assets/images/" + person.image
+                })
+        else:
             return None
-
-
--- src/models/person/person.py
+++ src/models/person/person.py
@@ -6,6 +6,7 @@ import src.models.person.constants as PersonConstants
 from src.common.database import Database
 from src.common.Utility.Utility import CommonUtility as PersonUtility
 
+
 class Person:
     def __init__(self, aadhaar_no, image, name, address, gender, dob, phone, fingerprint=None, _id=None):
         self._id = uuid.uuid4().hex if _id is None else _id
@@ -13,7 +14,7 @@ class Person:
         self.name = PersonUtility.formating_name(name)
         self.address = address
         self.gender = gender
-        self.dob = datetime.datetime.strptime(dob,"%Y-%m-%d") if isinstance(dob, str) else dob
+        self.dob = datetime.datetime.strptime(dob, "%Y-%m-%d") if isinstance(dob, str) else dob
         self.phone = PersonUtility.formating_phone(phone)
         self.fingerprint = fingerprint
         self.image = image
@@ -23,7 +24,7 @@ class Person:
             "_id": self._id,
             "aadhaar_no": self.aadhaar_no,
             "name": self.name,
-            "gender":self.gender,
+            "gender": self.gender,
             "address": self.address,
             "dob": self.dob.strftime("%Y-%m-%d"),
             "phone": self.phone,
@@ -35,17 +36,16 @@ class Person:
         Database.update(PersonConstants.COLLECTION, {"_id": self._id}, self.json())
 
     @classmethod
-    def get_by_id(cls,person_id):
-        data = Database.find_one(PersonConstants.COLLECTION,{'_id':person_id})
+    def get_by_id(cls, person_id):
+        data = Database.find_one(PersonConstants.COLLECTION, {'_id': person_id})
         return cls(**data) if data is not None else False
 
     @classmethod
     def get_all_user(cls):
-        cluster_data = Database.find(PersonConstants.COLLECTION,{})
+        cluster_data = Database.find(PersonConstants.COLLECTION, {})
         return [cls(**data) for data in cluster_data if cluster_data is not None] if cluster_data is not None else None
 
     @classmethod
     def get_by_aadhaar(cls, aadhaar_no):
-        data = Database.find_one(PersonConstants.COLLECTION, {'aadhaar_no':aadhaar_no})
+        data = Database.find_one(PersonConstants.COLLECTION, {'aadhaar_no': aadhaar_no})
         return cls(**data) if data is not None else None
-
